085e258c1aae95bea18e13307e393c2d74173e7a
Valentina Lykhopiy <valykho@gmail.com>
2018-10-25 14:25:02 +0200

    added .gitignore and scripts utils.py

---
+++ scripts/utils.py
@@ -0,0 +1,7 @@
+def print_hello():
+    """This function provides me a hello."""
+    print("hello")
+    
+def print_goodbye():
+    """This function provides me a goodbye."""
+    print("goodbye")
